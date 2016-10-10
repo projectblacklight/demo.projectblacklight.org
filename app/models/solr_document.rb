424586f6531cf12ff50a8c99bdc4def60191072c
Chris Beer <chris@cbeer.info>
2016-10-10 15:16:20 +0000

    Regenerate blacklight and blacklight-marc

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,6 +1,5 @@
-# -*- encoding : utf-8 -*-
-class SolrDocument 
-
+# frozen_string_literal: true
+class SolrDocument
   include Blacklight::Solr::Document    
       # The following shows how to setup this blacklight document to display marc documents
   extension_parameters[:marc_source_field] = :marc_display
@@ -19,18 +18,17 @@ class SolrDocument
 
 
   # self.unique_key = 'id'
-  
+
   # Email uses the semantic field mappings below to generate the body of an email.
-  SolrDocument.use_extension( Blacklight::Document::Email )
-  
+  SolrDocument.use_extension(Blacklight::Document::Email)
+
   # SMS uses the semantic field mappings below to generate the body of an SMS email.
-  SolrDocument.use_extension( Blacklight::Document::Sms )
+  SolrDocument.use_extension(Blacklight::Document::Sms)
 
   # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
   # Semantic mappings of solr stored fields. Fields may be multi or
   # single valued. See Blacklight::Document::SemanticFields#field_semantics
   # and Blacklight::Document::SemanticFields#to_semantic_values
   # Recommendation: Use field names from Dublin Core
-  use_extension( Blacklight::Document::DublinCore)    
-
+  use_extension(Blacklight::Document::DublinCore)
 end
