cf41bbc66ab141f437dab4b13759f576df19600a
Chris Beer <chris@cbeer.info>
2017-08-15 09:04:43 -0700

    Fixes for blacklight-marc and i18n localization

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module DemoProjectblacklightOrg
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.available_locales = %w(de en es fr it pt-BR sq)
   end
 end
