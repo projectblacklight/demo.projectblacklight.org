59ee72d8433152dbc202a40ceb53467bd7097915
Chris Beer <chris@cbeer.info>
2017-08-15 09:04:43 -0700

    Update dependencies

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,11 @@ Bundler.require(*Rails.groups)
 
 module DemoProjectblacklightOrg
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.available_locales = %w(de en es fr it pt-BR sq)
   end
 end
