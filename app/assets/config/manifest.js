f9fca83191b6b329787a5ae5292af45885a40059
Justin Coyne <jcoyne@justincoyne.com>
2024-10-14 10:21:03 -0500

    Add cssbundling-rails

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link blacklight/manifest.js
+//= link_tree ../builds
